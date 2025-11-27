d9c54569bf7f33bc015023b1a7b52e301136c9f7
Ranji <ranjinie.souria@gmail.com>
2025-11-27 18:48:29 +0100

    Basic html

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import logo from './assets/logo.png';
+import './App.css'
+
+function App() {
+
+  return (
+  <>   
+  <div className="navbar">
+    <img className="logo" src={logo} alt="Logo" />
+    <div className="main">
+      <h1>Welcome to Video Club</h1>
+      Convert any video to your desired format :
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
